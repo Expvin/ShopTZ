3765fbe6239f1f4c0ffbccdb0cbcab620790fb42
Oleg <olegmanu9@gmail.com>
2023-02-23 21:15:36 +0300

    Create SingIn layout

---
+++ app/src/main/java/com/expv1n/onlineshop/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.expv1n.onlineshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
