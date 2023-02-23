9045ff8ba96482ef5040fa4482b4ae447cf124d4
Oleg <olegmanu9@gmail.com>
2023-02-24 00:23:26 +0300

    Create Login screen

---
+++ app/src/main/java/com/expv1n/onlineshop/SignInPageFragment.kt
@@ -0,0 +1,23 @@
+package com.expv1n.onlineshop
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SignInPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in_page, container, false)
+    }
+
+    companion object {
+
+    }
+}
