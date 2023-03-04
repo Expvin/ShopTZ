cdeb81f8c3afe7eb9a9894e3b1bfb412a9044a37
Oleg <olegmanu9@gmail.com>
2023-03-05 01:36:51 +0300

    final version

--- app/src/main/java/com/expv1n/onlineshop/presentation/MainActivity.kt
+++ app/src/main/java/com/expv1n/onlineshop/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.expv1n.onlineshop.R
 import com.expv1n.onlineshop.databinding.ActivityMainBinding
 
@@ -15,6 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.BottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    launchFragment(Page1Fragment.getInstance(), Page1Fragment.NAME)
+                    true
+                }
+                R.id.profile -> {
+                    launchFragment(ProfileFragment.getInstance(), ProfileFragment.NAME)
+                    true
+                }
+                else -> true
+            }
+        }
+    }
+
+    private fun launchFragment(fragment: Fragment, fragmentName: String) {
+        this.supportFragmentManager.beginTransaction()
+            .replace(R.id.mainFragmentContainerView, fragment)
+            .addToBackStack(fragmentName)
+            .commit()
     }
 
     fun showBottomNavigationView() {
